b9531e8d84f03884d4d9e1d8f3ff82ff1b438779
MirceaTT8 <mircea.munteanu356@gmail.com>
2024-09-25 23:07:09 +0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/presentation/theme/Theme.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun MyApplicationTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        content = content
+    )
+}
